c106b399015f6a388ea5d94fa6c517a24f892977
Marcus Carroll <lobo.carroll@gmail.com>
2025-03-24 08:00:59 -0700

    product detail and image component

--- prostore/lib/actions/product.actions.ts
+++ prostore/lib/actions/product.actions.ts
@@ -1,16 +1,26 @@
 'use server'
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 // create a function that fetches the latest products in the form of a prisma object. convert to a js object.
 export async function getLatestProducts() {
-    const prisma = new PrismaClient();
+    
     const data = await prisma.product.findMany({
        take: LATEST_PRODUCTS_LIMIT,
        orderBy: {
            createdAt: 'desc'
        }
+    });
+    return convertToPlainObject(data);
+}
+
+// Get single product by its slug
+export async function getProductBySlug(slug: string) {
+    const data = await prisma.product.findFirst({
+        where: {
+            slug: slug
+        }
     });
     return convertToPlainObject(data);
 }

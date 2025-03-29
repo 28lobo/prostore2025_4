29408051d3c4cb1902bb9d2d5dad440993a1c69a
Marcus Carroll <lobo.carroll@gmail.com>
2025-03-29 09:42:04 -0700

    Credentials sign in form

--- prostore/app/(auth)/sign-in/page.tsx
+++ prostore/app/(auth)/sign-in/page.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next";
 import Link from "next/link";
 import Image from "next/image"
 import { APP_NAME } from "@/lib/constants";
+import CredentialsSignInForm from "./credentials-signin-form";
 
 export const metadata: Metadata = {
   title: 'Sign In',
@@ -13,7 +14,7 @@ const SignInPage = () => {
     return ( <div className="w-full max-w-md mx-auto p-4">
         <Card>
             <CardHeader className="space-y-4">
-                <Link href='/' className="">
+                <Link href='/' className="flex justify-center items-center">
                     <Image src="/images/logo.svg" width={100} height={100} alt={`${APP_NAME} logo`} priority={true}/>
                 </Link>
                 <CardTitle className="text-center">
@@ -22,11 +23,11 @@ const SignInPage = () => {
                 <CardDescription className="text-center">
                     Welcome back! Please sign in to your account.
                     <br/>
-                    Don&#39;t have an account? <Link href="/auth/sign-up" className="text-blue-500 hover:underline">Sign Up</Link>
+                   
                 </CardDescription>
             </CardHeader>
             <CardContent className="space-y-4">
-                {/* form here */}
+                <CredentialsSignInForm />
             </CardContent>
         </Card>
     </div> );

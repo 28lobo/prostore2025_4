b43517551c23688d8451d42efd3d8efc6b4b1e5d
Marcus Carroll <lobo.carroll@gmail.com>
2025-04-01 10:09:53 -0700

    callback url and redirect

--- prostore/app/(auth)/sign-in/page.tsx
+++ prostore/app/(auth)/sign-in/page.tsx
@@ -12,10 +12,15 @@ export const metadata: Metadata = {
   description: 'Authentication',
 }
 
-const SignInPage = async () => {
+const SignInPage = async (props: {
+    searchParams: Promise<{
+        callbackUrl: string;
+    }>
+}) => {
+    const { callbackUrl } = await props.searchParams
     const session = await auth()
     if (session) {
-        redirect('/')
+        redirect(callbackUrl || '/')
     }
     return ( <div className="w-full max-w-md mx-auto p-4">
         <Card>

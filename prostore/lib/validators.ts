83a9bffedb00c97a6c83e4dc9f9bb72ea3b8097b
Marcus Carroll <lobo.carroll@gmail.com>
2025-04-09 13:33:37 -0700

    Cart zod schema and cart model

--- prostore/lib/validators.ts
+++ prostore/lib/validators.ts
@@ -37,3 +37,26 @@ export const signUpFormSchema = z.object({
   message: "Passwords do not match",
   path: ["confirmPassword"],
 });
+
+
+// Cart schema
+export const cartItemSchema = z.object({
+  productId: z.string().min(1, "Product ID is required"),
+  name: z.string().min(1, "Name is required"),
+  slug: z.string().min(1, "Slug is required"),
+  qty: z.number().int().nonnegative("Quantity must be a non-negative integer"),
+  image: z.string().min(1, "Image is required"),
+  price: currency,
+
+});
+
+// insert cart schema
+export const insertCartSchema = z.object({
+  items: z.array(cartItemSchema),
+  itemsPrice: currency,
+  totalPrice: currency,
+  shippingPrice: currency,
+  taxPrice: currency,
+  sessionCartId: z.string().min(1, "Session cart ID is required"),
+  userId: z.string().optional().nullable(),
+});
--- prostore/types/index.ts
+++ prostore/types/index.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { insertProductSchema } from "@/lib/validators";
+import { insertProductSchema, insertCartSchema, cartItemSchema } from "@/lib/validators";
 
 // Infer the zod schema to get the type
 export type Product = z.infer<typeof insertProductSchema>&{
@@ -7,3 +7,7 @@ export type Product = z.infer<typeof insertProductSchema>&{
     rating: string;
     createdAt: Date;
 }
+
+export type Cart = z.infer<typeof insertCartSchema>
+
+export type CartItem = z.infer<typeof cartItemSchema>

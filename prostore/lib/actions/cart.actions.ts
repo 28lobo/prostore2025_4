e002cbd60d5c50a0feba96eb171d78c0d5c6dc31
Marcus Carroll <lobo.carroll@gmail.com>
2025-04-11 08:01:06 -0700

    fixed data

--- prostore/lib/actions/cart.actions.ts
+++ prostore/lib/actions/cart.actions.ts
@@ -3,8 +3,9 @@
 import { CartItem } from "@/types"
 
 export async function addItemToCart(data: CartItem) {
+    console.log(data)
     return {
         success: true,
-        message: 'Item added to cart',
+        message: 'Item added to cart',  
     }
 }

5f7c18ff3ea8614e68d7d340ad01a8647122e492
Marcus Carroll <lobo.carroll@gmail.com>
2025-04-26 10:28:18 -0700

    payment method page

---
+++ prostore/app/(root)/payment-method/page.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from "next";
+// get the session and user
+import { auth } from "@/auth";
+// get user's payment methods
+import { getUserById } from "@/lib/actions/user.actions";
+import PaymentMethodForm from "./payment-method-form";
+
+export const metadata: Metadata = {
+    title: "Select Payment Method",
+    keywords: ["payment", "method", "select", "checkout"],
+    description: "Payment Method",
+};
+
+const PaymentMethodPage = async() => {
+    const session = await auth();
+    const userId = session?.user.id;
+
+    if(!userId) throw new Error("User not found");
+    const user = await getUserById(userId);
+    return ( <>
+        <PaymentMethodForm preferredPaymentMethod={user?.paymmentMethod} />
+    </> );
+}
+ 
+export default PaymentMethodPage;

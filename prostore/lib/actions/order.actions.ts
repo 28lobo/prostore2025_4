87bfaa9497084d173ada1e77cfba02a2305f05de
Marcus Carroll <lobo.carroll@gmail.com>
2025-05-01 07:57:20 -0700

    order actions

---
+++ prostore/lib/actions/order.actions.ts
@@ -0,0 +1,95 @@
+"use server";
+
+import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { formatError } from "../utils";
+import { auth } from "@/auth";
+import { getMyCart } from "./cart.actions";
+import { getUserById } from "./user.actions";
+import { insertOrderSchema } from "../validators";
+import { prisma } from "@/db/prisma";
+import type { CartItem } from "@/types";
+
+// Create an order and the order items
+export async function createOrder() {
+  try {
+    const session = await auth();
+    if (!session) throw new Error("User not Authenticated");
+    // need cart items
+    const cart = await getMyCart();
+    // user id
+    const userId = session?.user?.id;
+    // check if there is a userid
+    if (!userId) throw new Error("User not found");
+    // get user from database using the user id from the session. This gives us the address, payment, etc
+    const user = await getUserById(userId);
+    // redirect if cart is empty
+    if (!cart || cart.items.length === 0) {
+      return {
+        success: false,
+        message: "Your cart is empty",
+        redirectTo: "/cart",
+      };
+    }
+    if (!user || !user.address) {
+      return {
+        success: false,
+        message: "No shipping address",
+        redirectTo: "/shipping-address",
+      };
+    }
+    if (!user.paymmentMethod) {
+      return {
+        success: false,
+        message: "No payment method",
+        redirectTo: "/payment-method",
+      };
+    }
+
+    // Create the order object.
+    const order = insertOrderSchema.parse({
+      userId: user.id,
+      shippingAddress: user.address,
+      paymentMethod: user.paymmentMethod,
+      itemsPrice: cart.itemsPrice,
+      shippingPrice: cart.shippingPrice,
+      taxPrice: cart.taxPrice,
+      totalPrice: cart.totalPrice,
+
+    });
+
+    // Create a transacttion that creates an order and the order items
+    const insertedOrderId = await prisma.$transaction(async (tx) => {
+        // create order
+        const insertedOrder = await tx.order.create({ data: order });
+        // create order items from the cart items
+        for (const item of cart.items as CartItem[]){
+            await tx.orderItem.create({
+                data: {
+                    ...item,
+                    price: item.price,
+                    orderId: insertedOrder.id
+                },
+            });
+        }
+        // clear the cart
+        await tx.cart.update({
+            where: {
+                id: cart.id,
+            },
+            data: {
+                items: [],
+                totalPrice: 0,
+                taxPrice: 0,
+                shippingPrice: 0,
+                itemsPrice: 0,  
+            },
+        });
+        return insertedOrder.id
+    })
+    if(!insertedOrderId) throw new Error("Failed to create order");
+    return { success: true, message: "Order created successfully", redirectTo: `/order/${insertedOrderId}`};
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+    return { success: false, message: formatError(error) };
+  }
+}

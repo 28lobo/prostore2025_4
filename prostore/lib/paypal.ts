298b6106a645bb1932d1a9e5f07445c9b3907e7b
Marcus Carroll <lobo.carroll@gmail.com>
2025-05-03 09:21:32 -0700

    generate access token

---
+++ prostore/lib/paypal.ts
@@ -0,0 +1,35 @@
+// gets the paypal sandbox
+const base = process.env.PAYPAY_API_URL || "https://api-m.sandbox.paypal.com";
+
+// create paypal order and capture payment
+export const paypal = {}
+
+
+// generate paypal access token
+
+async function generateAccessToken () {
+    // get client id and secret
+    const {PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET} = process.env;
+    // Put client id and secret in base64
+    const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString("base64");
+    // send the request in header
+    const response = await fetch(`${base}/v1/oauth2/token`, {
+        method: "POST",
+        body: "grant_type=client_credentials",
+        headers: {
+            Authorization: `Basic ${auth}`,
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+    });
+    // check if response is true
+    if(response.ok){
+        const jsonData = await response.json();
+        return jsonData.access_token;
+    
+    }else {
+        const errorMessage = await response.text()
+        throw new Error(errorMessage);
+    }
+}
+
+generateAccessToken()

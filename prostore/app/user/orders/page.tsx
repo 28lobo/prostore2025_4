936f61956e70c5c259e28c7b63595c65289aa8bb
Marcus Carroll <lobo.carroll@gmail.com>
2025-05-09 10:34:22 -0700

    order page & changed the user button

--- prostore/app/user/orders/page.tsx
+++ prostore/app/user/orders/page.tsx
@@ -1,5 +1,69 @@
-const OrdersPage = () => {
-    return ( <>Orders</> );
-}
- 
+import { Metadata } from "next";
+import { getMyOrders } from "@/lib/actions/order.actions";
+import { formatCurrency, formatDateTime, formatId } from "@/lib/utils";
+import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableCell,
+} from "@/components/ui/table";
+
+// metadata
+export const metadata: Metadata = {
+  title: "Orders",
+  description: "Manage your orders",
+};
+
+const OrdersPage = async (props: {
+  searchParams: Promise<{ page: string }>;
+}) => {
+  const { page } = await props.searchParams;
+
+  // get orders. The getMyOrders() takes in a page and a limit. The argument for the function is an object with the property page and value number(page)
+  const orders = await getMyOrders({
+    page: Number(page) || 1,
+  });
+
+  return (
+    <div className="space-y-2">
+      <h2 className="font-bold">Orders</h2>
+      <div className="overflow-x-auto">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>DATE</TableHead>
+              <TableHead>TOTAL</TableHead>
+              <TableHead>PAID</TableHead>
+              <TableHead>DELIVERED</TableHead>
+              <TableHead>ACTIONS</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {orders.data.map((order) => (
+              <TableRow key={order.id}>
+                <TableCell>{formatId(order.id)}</TableCell>
+                <TableCell>{formatDateTime(order.createdAt).dateTime}</TableCell>
+                <TableCell>{formatCurrency(order.totalPrice)}</TableCell>
+                <TableCell>{order.isPaid && order.paidAt ? formatDateTime(order.paidAt).dateTime : 'Not Paid'}</TableCell>
+                <TableCell>{order.isDelivered && order.deliveredAt ? formatDateTime(order.deliveredAt).dateTime : 'Not delivered'}</TableCell>
+                <TableCell>
+                    <Link href={`/order/${order.id}`}>
+                        <span className="px-2">
+                            Details
+                        </span>
+                    </Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+
 export default OrdersPage;
--- prostore/components/shared/header/user-button.tsx
+++ prostore/components/shared/header/user-button.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useSession } from "next-auth/react"; // or your custom hook
 import Link from "next/link";
@@ -7,6 +7,7 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { UserIcon } from "lucide-react";
@@ -24,7 +25,8 @@ const UserButton = () => {
     );
   }
 
-  const firstInitial = session.user?.name?.charAt(0).toUpperCase() ?? "U";
+  const firstInitial =
+    (session.user as { name?: string })?.name?.charAt(0).toUpperCase() ?? "U";
 
   return (
     <div className="flex gap-2 items-center">
@@ -38,13 +40,32 @@ const UserButton = () => {
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-56" align="end" forceMount>
-          <div className="font-normal px-2 py-1">
-            <div className="text-sm font-medium">{session.user?.name}</div>
-            <div className="text-muted-foreground text-sm">{session.user?.email}</div>
-          </div>
+          <DropdownMenuLabel>
+            <div className="font-normal px-2 py-1">
+              <div className="text-sm font-medium">
+                {(session.user as { name?: string })?.name}
+              </div>
+              <div className="text-muted-foreground text-sm">
+                {session.user?.email}
+              </div>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuItem>
+            <Link href='/users/profile' className="w-full">
+              User Profile
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href='/users/order' className="w-full">
+              Order History
+            </Link>
+          </DropdownMenuItem>
           <DropdownMenuItem className="p-0 mb-1">
             <form action="/api/auth/signout" method="POST" className="w-full">
-              <Button className="w-full justify-start py-4 px-1 h-4" variant="ghost">
+              <Button
+                className="w-full justify-start py-4 px-1 h-4"
+                variant="ghost"
+              >
                 Sign Out
               </Button>
             </form>

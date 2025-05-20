3fa900f8882fc8a313e08a55c50955d863788c0c
Marcus Carroll <lobo.carroll@gmail.com>
2025-05-20 08:18:16 -0700

    COD actions

--- prostore/lib/actions/order.actions.ts
+++ prostore/lib/actions/order.actions.ts
@@ -398,3 +398,51 @@ export async function deleteOrder(id:string){
     return { success: false, message: formatError(error) };
   }
 }
+
+// update order to paid COD
+export async function updateOrderToPaidCOD(orderId: string){
+  try {
+    await updateOrderToPaid({
+      orderId
+    })
+    revalidatePath(`/order/${orderId}`);
+
+    return {
+      success: true,
+      message: "Order paid successfully",
+    };
+
+  }catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// update COD order to delivered
+export async function deliverOrder(orderId: string){
+  try {
+    const order = await prisma.order.findFirst({
+      where: {
+        id: orderId,
+      },
+    })
+    if (!order) throw new Error("Order not found");
+
+    if(!order.isPaid) throw new Error("Order is not paid");
+    await prisma.order.update({
+      where: {
+        id: orderId,
+      },
+      data: {
+        isDelivered: true,
+        deliveredAt: new Date(),
+      },
+    });
+    revalidatePath(`/order/${orderId}`);
+    return {
+      success: true,
+      message: "Order delivered successfully",
+    };
+  }catch (error){
+    return { success: false, message: formatError(error) };
+  }
+}
